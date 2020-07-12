84f26470fd0c88c1d913e857afc3a4eb315c3d3f
leastbad <hello@leastbad.com>
2020-07-12 05:53:12 -0400

    work so far

--- lib/futurism/helpers.rb
+++ lib/futurism/helpers.rb
@@ -1,13 +1,13 @@
 module Futurism
   module Helpers
-    def futurize(records, extends:, &block)
+    def futurize(records, extends:, **options, &block)
       placeholder = capture(&block)
       Array(records).map { |record|
         case extends
         when :tr
-          content_tag :tr, placeholder, data: {sgid: record.to_sgid.to_s}, is: "futurism-table-row"
+          content_tag :tr, placeholder, {data: {sgid: record.to_sgid.to_s}, is: "futurism-table-row"}.merge(options)
         else
-          content_tag :"futurism-element", placeholder, data: {sgid: record.to_sgid.to_s}
+          content_tag :"futurism-element", placeholder, {data: {sgid: record.to_sgid.to_s}}.merge(options)
         end
       }.join.html_safe
     end
