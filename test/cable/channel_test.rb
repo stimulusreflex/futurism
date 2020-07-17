a049fc948c45a483b8fb81c2b5112dbfdef4f9a5
Julian Rubisch <julian@julianrubisch.at>
2020-07-17 15:36:33 +0200

    Add test for ActiveRecord::Relation

--- test/cable/channel_test.rb
+++ test/cable/channel_test.rb
@@ -22,6 +22,18 @@ class Futurism::ChannelTest < ActionCable::Channel::TestCase
     assert renderer_spy.has_been_called_with? post
   end
 
+  test "broadcasts an ActiveRecord::Relation" do
+    renderer_spy = Spy.on(ApplicationController, :render)
+    Post.create title: "Lorem"
+    Post.create title: "Ipsum"
+    signed_params = futurism_signed_params(Post.all)
+    subscribe
+
+    perform :receive, {"signed_params" => [signed_params]}
+
+    assert renderer_spy.has_been_called_with? Post.all
+  end
+
   test "broadcasts a rendered partial after receiving signed params" do
     renderer_spy = Spy.on(ApplicationController, :render)
     post = Post.create title: "Lorem"
