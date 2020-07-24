16ade6ad40fa8ee40efa99c8c892383fe8bc88dc
leastbad <hello@leastbad.com>
2020-07-24 17:33:46 -0400

    enable passing of html_options hash

--- lib/futurism/helpers.rb
+++ lib/futurism/helpers.rb
@@ -4,7 +4,7 @@ module Futurism
       placeholder = capture(&block)
 
       if records_or_string.is_a?(ActiveRecord::Base) || records_or_string.is_a?(ActiveRecord::Relation)
-        futurize_active_record(records_or_string, extends: extends, placeholder: placeholder)
+        futurize_active_record(records_or_string, extends: extends, placeholder: placeholder, **options)
       elsif records_or_string.is_a?(String)
         futurize_with_options(extends: extends, partial: records_or_string, locals: options, placeholder: placeholder)
       else
@@ -24,20 +24,22 @@ module Futurism
       end
     end
 
-    def futurize_active_record(records, extends:, placeholder:)
+    def futurize_active_record(records, extends:, placeholder:, **options)
       Array(records).map { |record|
-        render_element(extends: extends, placeholder: placeholder, options: record)
+        render_element(extends: extends, placeholder: placeholder, options: options.merge(model: record))
       }.join.html_safe
     end
 
     def render_element(extends:, options:, placeholder:)
+      html_options = options.delete(:html_options) || {}
+      options = options.delete(:model) || options
       case extends
       when :li
-        content_tag :li, placeholder, data: {signed_params: futurism_signed_params(options)}, is: "futurism-li"
+        content_tag :li, placeholder, {data: {signed_params: futurism_signed_params(options)}, is: "futurism-li"}.merge(html_options)
       when :tr
-        content_tag :tr, placeholder, data: {signed_params: futurism_signed_params(options)}, is: "futurism-table-row"
+        content_tag :tr, placeholder, {data: {signed_params: futurism_signed_params(options)}, is: "futurism-table-row"}.merge(html_options)
       else
-        content_tag :"futurism-element", placeholder, data: {signed_params: futurism_signed_params(options)}
+        content_tag :"futurism-element", placeholder, {data: {signed_params: futurism_signed_params(options)}}.merge(html_options)
       end
     end
 
