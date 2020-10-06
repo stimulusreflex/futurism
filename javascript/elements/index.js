37512cbe42af90b6f317836c1cd88cc134021411
Julian Rubisch <julian@julianrubisch.at>
2020-10-06 09:08:54 +0200

    Namespace sessionStorage keys #23

--- javascript/elements/index.js
+++ javascript/elements/index.js
@@ -39,22 +39,29 @@ const defineElements = e => {
 const cachePlaceholders = e => {
   sha256(e.detail.element.outerHTML).then(hashedContent => {
     e.detail.element.setAttribute('keep', '')
-    sessionStorage.setItem(hashedContent, e.detail.element.outerHTML)
+    sessionStorage.setItem(
+      `futurism-${hashedContent}`,
+      e.detail.element.outerHTML
+    )
     e.target.dataset.futurismHash = hashedContent
   })
 }
 
 const restorePlaceholders = e => {
-  Object.entries(sessionStorage).forEach(([key, payload]) => {
-    const targetElement = document.querySelector(
-      `[data-futurism-hash="${key}"]`
-    )
+  const inNamespace = ([key, _payload]) => key.startsWith('futurism-')
+  Object.entries(sessionStorage)
+    .filter(inNamespace)
+    .forEach(([key, payload]) => {
+      const match = /^futurism-(.*)/.exec(key)
+      const targetElement = document.querySelector(
+        `[data-futurism-hash="${match[1]}"]`
+      )
 
-    if (targetElement) {
-      targetElement.outerHTML = payload
-      sessionStorage.removeItem(key)
-    }
-  })
+      if (targetElement) {
+        targetElement.outerHTML = payload
+        sessionStorage.removeItem(key)
+      }
+    })
 }
 
 export const initializeElements = () => {
