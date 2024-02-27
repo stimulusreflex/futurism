ee725326d9452268ee3debfe59c5259f4c4d0c93
Julian Rubisch <julian@julianrubisch.at>
2024-02-27 09:06:12 +0100

    fix: Set message verifier serializer to Marshal

--- lib/futurism/message_verifier.rb
+++ lib/futurism/message_verifier.rb
@@ -1,11 +1,11 @@
 module Futurism
   module MessageVerifier
     def self.message_verifier
-      @message_verifier ||= Rails.application.message_verifier("futurism")
+      @message_verifier ||= ActiveSupport::MessageVerifier.new(Rails.application.key_generator.generate_key("futurism/verifier_key"), digest: "SHA256", serializer: Marshal)
     end
 
     def message_verifier
-      @message_verifier ||= Rails.application.message_verifier("futurism")
+      @message_verifier ||= Futurism::MessageVerifier.message_verifier
     end
   end
 end
